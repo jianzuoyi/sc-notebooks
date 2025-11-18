53e706158a949ff9261a0deb3b371cd2b28bda95
jianzuoyi <543355797@qq.com>
2025-11-18 08:44:25 +0800

    mv debug code to ipynb

--- scanpy/preprocessing_simple_def_filter_cells.py
+++
@@ -1,8 +0,0 @@
-import scanpy as sc
-
-breakpoint()
-adata = sc.datasets.krumsiek11()
-adata.obs_names_make_unique()
-#sc.pp.filter_cells(adata, min_genes=3)
-sc.pp.filter_genes(adata, min_cells=3)
-adata
